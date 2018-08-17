ccbd509c244c17231aba4d7a227f1c40fb416450
alwaysdying <42467293+alwaysdying@users.noreply.github.com>
2018-08-17 13:12:51 +0200

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1751,7 +1751,7 @@ void* miningThread(void *x_void_ptr) {
     unsigned int lo,hi;
     __asm__ __volatile__ ("rdtsc" : "=a" (lo), "=d" (hi));
     return ((unsigned long long)hi << 32) | lo;
-}/*
+}*/
 
 int main(int argc, const char * argv[]) {
     
